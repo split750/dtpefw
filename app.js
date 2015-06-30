71b609b96887e894c40f4360b154d1ca48bf7917
split750 <godefroy.lemerdy@gmail.com>
2015-06-30 17:34:34 +0200

    change port configuration

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var app = require("./server/routes");
 
+var server = require('http').createServer(app);
+var port = process.env.PORT || 1337;
+
 // Start the server
-var server = app.listen(8000, function() {
- console.log('Listening on port %d', server.address().port);
+server.listen(port, function() {
+ console.log('Listening on port : ' + port);
 });
