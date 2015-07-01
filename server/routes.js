cca4fdeb4b9344fa50ab9190db13b053d7797dcd
split750 <godefroy.lemerdy@gmail.com>
2015-07-01 19:45:28 +0200

    change settings

--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ require('./expressConfig')(app, express);
 
 // Root route
 app.get('/', function(req, res){
-  res.sendfile('index.html' /*, {root: app.settings.views}*/);
+  res.sendfile('index.html', {root: app.settings.views});
   console.log(app.settings.views);
 });
 
