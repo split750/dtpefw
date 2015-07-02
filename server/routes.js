41ed72aca744dc4de1699765ce5496e70e877185
split750 <godefroy.lemerdy@gmail.com>
2015-07-02 14:40:19 +0200

    npm install and configuration express change

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // Load Express Configuration
 require('./expressConfig')(app, express);
 
 // Root route
 
-app.use(express.static(__dirname + '../app/assets/'));
+app.use(express.static(path.join(__dirname, '../app/')));
+
 
 app.get('/', function(req, res){
-  res.sendfile('./app/views/index.html');
+  res.sendFile('index.html', { root: path.join(__dirname, '../app/views') });
 });
+
 
 // Load routes
 require('./routes/user')(app); //user routes
