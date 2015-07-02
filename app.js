ed81c9723d6f61a28c3c14391a57e5d600c7a8dd
split750 <godefroy.lemerdy@gmail.com>
2015-07-02 17:19:11 +0200

    change to new app

---
+++ app.js
@@ -0,0 +1,41 @@
+
+//var app = require("./server/routes");
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+// Load Express Configuration
+require('./server/expressConfig')(app, express);
+
+
+// Load routes
+require('./server/routes/user')(app); //user routes
+require('./server/routes/note')(app); // note routes
+require('./server/routes/category')(app); // category routes
+require('./server/routes/wasteType')(app);
+require('./server/routes/contractType')(app);
+
+
+
+
+
+
+app.use(express.static(path.join(__dirname, './app/')));
+
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', { root: path.join(__dirname, './app/views') });
+});
+
+
+// Create server
+var port = process.env.PORT || 1337;
+var server = require('http').createServer(app);
+
+
+// Start the server
+server.listen(port, function() {
+ console.log('Listening on port : ' + port);
+});
