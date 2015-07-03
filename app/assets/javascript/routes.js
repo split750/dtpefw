a2d46c22584744c78f58fab11ffbaf3eaa3278e1
split750 <godefroy.lemerdy@gmail.com>
2015-07-03 09:41:57 +0200

    update routes app

--- app/assets/javascript/routes.js
+++ app/assets/javascript/routes.js
@@ -5,37 +5,37 @@ angular.module('NoteWrangler').config(function($routeProvider){
     })
 
     .when('/notes', {
-      templateUrl: "assets/templates/notes/index.html",
+      templateUrl: "app/assets/templates/notes/index.html",
       controller: "NotesIndexController"
     })
 
     .when('/notes/new', {
-      templateUrl: "assets/templates/notes/new.html",
+      templateUrl: "app/assets/templates/notes/new.html",
       controller: "NotesCreateController"
     })
 
     .when('/notes/:id', {
-      templateUrl: "assets/templates/notes/show.html",
+      templateUrl: "app/assets/templates/notes/show.html",
       controller: "NotesShowController"
     })
 
     .when('/notes/:id/edit', {
-      templateUrl: "assets/templates/notes/edit.html",
+      templateUrl: "app/assets/templates/notes/edit.html",
       controller: "NotesEditController"
     })
 
     .when('/users', {
-      templateUrl: "assets/templates/users/index.html",
+      templateUrl: "app/assets/templates/users/index.html",
       controller: "UsersIndexController"
     })
 
     .when('/users/:id', {
-      templateUrl: "assets/templates/users/show.html",
+      templateUrl: "app/assets/templates/users/show.html",
       controller: "UsersShowController"
     })
 
     .when('/about', {
-      templateUrl: "assets/templates/about.html",
+      templateUrl: "app/assets/templates/about.html",
       //controller: "AboutController"
     });
 
