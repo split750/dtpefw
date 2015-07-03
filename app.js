df23f79a26f114c7fe8a7f58f189c38d09835641
split750 <godefroy.lemerdy@gmail.com>
2015-07-03 12:05:55 +0200

    change express config

--- app.js
+++ app.js
@@ -19,14 +19,11 @@ require('./server/routes/contractType')(app);
 
 
 
-
-
-
 app.use(express.static(path.join(__dirname, './public/')));
 
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', { root: path.join(__dirname, './public/views') });
+  res.sendFile('index.html', { root: path.join(__dirname, './public/views/') });
 });
 
 
